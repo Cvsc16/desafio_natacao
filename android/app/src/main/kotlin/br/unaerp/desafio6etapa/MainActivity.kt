1a2d8ad45b08bf672d0ce5f454540f419dd6ca0c
mlcosta <caio.vcosta@sou.unaerp.edu.br>
2023-10-02 11:48:32 -0300

    02 10 23

---
+++ android/app/src/main/kotlin/br/unaerp/desafio6etapa/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.unaerp.desafio6etapa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  void _forgotPassword() {
+    // Adicione aqui a navegação para a tela de recuperação de senha
+    // Por exemplo: Navigator.push(context, MaterialPageRoute(builder: (context) => ForgotPasswordScreen()));
+  }
+
+  void _login() {
+    // Adicione aqui a lógica para efetuar o login
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            // Container com a imagem de fundo principal
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/fundo_login.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              height: MediaQuery.of(context).size.height + 500, // Defina a altura para ocupar toda a tela
+              width: MediaQuery.of(context).size.width, // Defina a largura para ocupar toda a tela
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/cima_fundo.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                height: 400,
+              ),
+            ),
+            // Conteúdo no centro da tela
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 40.0, top: 400.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextField(
+                        style: TextStyle(
+                          color: Colors.white, // Define a cor do texto digitado
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Open Sans',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.yellow,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF354170), width: 2.0),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.yellow, width: 2.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Open Sans',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF354170), width: 2.0),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscureText ? Icons.visibility : Icons.visibility_off,
+                              color: Colors.white,
+                            ),
+                            onPressed: _togglePasswordVisibility,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0), // Espaço adicional para o texto "Esqueci minha senha"
+                      GestureDetector(
+                        onTap: _forgotPassword, // Chama a função ao clicar no texto
+                        child: Text(
+                          'Esqueci minha senha',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.yellow,
+                          ),
+                          textAlign: TextAlign.right, // Alinha o texto à direita
+                        ),
+                      ),
+                      SizedBox(height: 40.0), // Espaço adicional para o botão "Login"
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 0.0),
+                        child: ElevatedButton(
+                          onPressed: _login, // Chama a função ao clicar no botão
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.yellow, // Cor de fundo amarela
+                            onPrimary: Color(0xFF0C2172), // Cor do texto "#0C2172"
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0), // Raio da borda de 50
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 200.0),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
