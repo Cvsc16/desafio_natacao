59e6426e30f39dc127f820af6943befe4e596bae
mlcosta <caio.vcosta@sou.unaerp.edu.br>
2023-10-05 11:45:14 -0300

    02 10 23

--- lib/screens/homeAtleta.dart
+++ lib/screens/homeAtleta.dart
@@ -1,16 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Treino extends StatelessWidget {
+  final String id_treino;
+  final String tipo_treino;
+  final String data_treino;
+  final String tempo_treino;
+
+  Treino({
+    required this.id_treino,
+    required this.tipo_treino,
+    required this.data_treino,
+    required this.tempo_treino,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              id_treino,
+              style: TextStyle(
+                fontSize: 15.0,
+                fontFamily: 'Open Sans',
+                fontWeight: FontWeight.w600,
+                color: Colors.yellow,
+              ),
+            ),
+            SizedBox(width: 5.0),
+            Expanded(
+              child: Text(
+                tipo_treino,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontFamily: 'Open Sans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Text(
+                tempo_treino,
+                style: TextStyle(
+                  fontSize: 13.0,
+                  fontFamily: 'Open Sans',
+                  fontWeight: FontWeight.w600,
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            SizedBox(width: 20.0),
+          ],
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: Container(
+                width: 2.0,
+                height: 11.0,
+                color: Colors.yellow,
+              ),
+            ),
+            SizedBox(width: 5.0),
+            Text(
+              data_treino,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontFamily: 'Open Sans',
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          width: 280.0,
+          height: 1.0,
+          color: Color(0xFF505050),
+        ),
+        SizedBox(height: 10.0),
+      ],
+    );
+  }
+}
 
 class HomeAtleta extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF0C2172), // Cor da borda superior
+        backgroundColor: Color(0xFF0C2172),
         toolbarHeight: 4.0,
         elevation: 0.0,
       ),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 60.0, 0.0, 0.0), // Espaçamento à esquerda e superior
+        padding: const EdgeInsets.fromLTRB(20.0, 60.0, 0.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -27,19 +118,19 @@ class HomeAtleta extends StatelessWidget {
                     color: Color(0xFF06113C),
                   ),
                 ),
-                SizedBox(width: 5.0), // Espaçamento entre os textos
+                SizedBox(width: 5.0),
                 Text(
                   'Sarah,',
                   style: TextStyle(
                     fontSize: 36.0,
                     fontFamily: 'Open Sans',
-                    fontWeight: FontWeight.w600, // Use FontWeight.w600 para negrito
+                    fontWeight: FontWeight.w600,
                     color: Color(0xFF06113C),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 5.0), // Espaçamento de 10 pixels abaixo
+            SizedBox(height: 5.0),
             Text(
               'Bons treinos!',
               style: TextStyle(
@@ -49,7 +140,7 @@ class HomeAtleta extends StatelessWidget {
                 color: Color(0xFF06113C),
               ),
             ),
-            SizedBox(height: 25.0), // Espaçamento de 25 pixels abaixo
+            SizedBox(height: 25.0),
             Text(
               'Meus Tempos',
               style: TextStyle(
@@ -59,113 +150,141 @@ class HomeAtleta extends StatelessWidget {
                 color: Color(0xFF0F2F7A),
               ),
             ),
-            SizedBox(height: 20.0), // Espaçamento de 20 pixels abaixo
-
+            SizedBox(height: 20.0),
             // Usar um Stack para envolver o retângulo azul e cinza
             Stack(
               children: [
                 Container(
                   width: 320.0,
-                  alignment: Alignment.center, // Alinhe o retângulo azul
+                  alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Color(0xFF0C2172), // Cor do retângulo azul
-                    borderRadius: BorderRadius.circular(16.0), // Raio da borda
+                    color: Color(0xFF0C2172),
+                    borderRadius: BorderRadius.circular(16.0),
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 18.0), // Espaçamento de 18 pixels do topo do retângulo azul
+                      SizedBox(height: 10.0),
                       Padding(
-                        padding: EdgeInsets.only(left: 20.0), // Espaçamento de 20 pixels da esquerda do retângulo azul
+                        padding: EdgeInsets.only(left: 20.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              children: [
-                                Text(
-                                  '1',
-                                  style: TextStyle(
-                                    fontSize: 15.0, // Tamanho do número
-                                    fontFamily: 'Open Sans',
-                                    fontWeight: FontWeight.w600, // Use FontWeight.w600 para negrito
-                                    color: Colors.yellow, // Cor do número
-                                  ),
-                                ),
-                                SizedBox(width: 5.0), // Espaçamento entre o número e o texto
-                                Expanded(
-                                  child: Text(
-                                    'Nado borboleta - 100m',
-                                    style: TextStyle(
-                                      fontSize: 15.0, // Tamanho do texto
-                                      fontFamily: 'Open Sans',
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white, // Cor do texto
-                                    ),
-                                  ),
-                                ),
-                              ],
+                            Treino(
+                              id_treino: '1',
+                              tipo_treino: 'Nado borboleta - 100m',
+                              data_treino: '04/10/2023',
+                              tempo_treino: '07:10:65',
                             ),
-                            SizedBox(height: 4.0), // Espaçamento de 4 pixels abaixo do texto
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 15.0), // Define o espaçamento de 8 pixels em todos os lados do Container
-                                  child: Container(
-                                    width: 2.0,
-                                    height: 11.0,
-                                    color: Colors.yellow, // Cor do retângulo amarelo
-                                  ),
-                                ),
-                                SizedBox(width: 5.0), // Espaçamento de 5 pixels à direita do retângulo amarelo
-                                Text(
-                                  '04/10/2023',
-                                  style: TextStyle(
-                                    fontSize: 12.0, // Tamanho do texto
-                                    fontFamily: 'Open Sans',
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.white, // Cor do texto
-                                  ),
-                                ),
-                                SizedBox(width: 140.0),
-                                Positioned(
-                                  right: 20.0, // Distância da direita do retângulo azul
-                                  top: 30.0, // Distância do topo do retângulo azul
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      '07:10:65',
-                                      style: TextStyle(
-                                        fontSize: 13.0, // Tamanho do texto
-                                        fontFamily: 'Open Sans',
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.yellow, // Cor do texto
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                            Treino(
+                              id_treino: '2',
+                              tipo_treino: 'Nado crawl - 50m',
+                              data_treino: '05/10/2023',
+                              tempo_treino: '08:15:42',
                             ),
-                            SizedBox(height: 8.0), // Espaçamento de 10 pixels abaixo do retângulo azul
-                            Container(
-                              width: 280.0,
-                              height: 1.0, // Altura de 1 pixel
-                              color: Color(0xFF505050), // Cor do retângulo cinza
-
+                            Treino(
+                              id_treino: '3',
+                              tipo_treino: 'Nado Borboleta - 100m',
+                              data_treino: '02/10/2023',
+                              tempo_treino: '10:15:42',
                             ),
-                            SizedBox(height: 10.0), // Espaçamento de 10 pixels abaixo do retângulo azul
                           ],
                         ),
                       ),
+                      SizedBox(height: 10.0),
                     ],
                   ),
-
                 ),
-
               ],
             ),
+            SizedBox(height: 20.0),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Média',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF0F2F7A),
+                      ),
+                    ),
+                    Spacer(), // Isso fará com que o retângulo ocupe o espaço restante à direita
+                    Container(
+                      width: 140.0,
+                      height: 30.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: Color(0xFFE3E3E3),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround, // Alinhar os textos horizontalmente no espaço disponível
+                        children: [
+                          // Valor "50m"
+                          Container(
+                            width: 36.0,
+                            height: 20.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5.0),
+                              color: Color(0xFF0C2172), // Cor azul de seleção
+                            ),
+                            child: Center(
+                              child: Text(
+                                '50m',
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontFamily: 'Open Sans',
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white, // Texto em branco para indicar seleção
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // Valor "100m"
+                          Text(
+                            '100m',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF0F2F7A), // Cor padrão
+                            ),
+                          ),
+
+                          // Valor "500m"
+                          Text(
+                            '500m',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF0F2F7A), // Cor padrão
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20.0),
+                  ],
+                ),
+                SizedBox(height: 40.0), // Espaçamento de 20 pixels entre o Row e o SVG
+                Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/grafico.svg',
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+
